0d572975ba70e6c85c546ca59ee627d7045c2bf8
Ali Hammoud <ali.hammoud.lb@gmail.com>
2016-04-25 18:13:15 +0300

    Added Sample Unity Project

---
+++ TestUnityProj/Assets/Scripts/Hello.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hello : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log("Hello");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
